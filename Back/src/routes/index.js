25a6fc2e5bf06ca91c2d01537ae6848c877f868d
AnaliaGiachini <analiagiachini@gmail.com>
2023-04-17 12:24:31 +0200

    Create-Routes-Donations

--- Back/src/routes/index.js
+++ Back/src/routes/index.js
@@ -4,11 +4,13 @@ const usersRoute = require("./usersRoute")
 const opinionsRoute = require("./opinionsRoute")
 const postsRoute = require('./postsRoute')
 const articlesRoute = require('./articlesRoute')
+const donationsRoute = require('./donationsRoute')
 
 router.use("/dogs", dogsRoute)
 router.use("/users", usersRoute)
 router.use("/opinions", opinionsRoute)
 router.use('/posts', postsRoute)
 router.use('/articles', articlesRoute)
+router.use('/donations', donationsRoute)
 
 module.exports = router
