5112967885e8441a4856653b21d50ca0a9fc3c5d
EliasB7 <113938831+EliasB7@users.noreply.github.com>
2023-04-23 19:52:48 -0300

    CambiosCloduinary

--- Back/src/controllers/imagesController.js
+++ Back/src/controllers/imagesController.js
@@ -18,7 +18,7 @@ const uploadImage = async (imagePath) => {
   try {
     const result = await cloudinary.uploader.upload(imagePath, options);
     console.log(result);
-    return result.public_id;
+    return result.secure_url;
   } catch (error) {
     console.error(error);
   }
--- Back/src/handlers/imagesHandler.js
+++ Back/src/handlers/imagesHandler.js
@@ -2,8 +2,8 @@ const { uploadImage } = require("../controllers/imagesController");
 
 const uploadImageHandler = async (imagePath) => {
   try {
-    const publicId = await uploadImage(imagePath);
-    return { success: true, publicId };
+    const urlImage = await uploadImage(imagePath);
+    return { success: true, urlImage };
   } catch (error) {
     console.error(error);
     return { success: false, message: "Failed to upload image" };
