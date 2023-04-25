57a1e5f5f8091a2247cb4f98c8dde7909c9ff92d
pablo herrera <valeherreradelarua@gmail.com>
2023-04-25 20:24:31 -0300

    Fix adoptionController,adoptionRoute

--- Back/src/routes/adoptionRoute.js
+++ Back/src/routes/adoptionRoute.js
@@ -10,10 +10,10 @@ const {
 } = require('../handlers/adoptionHandler')
 
 // Routes
-articlesRoute.post('/register', createAdoptionHandler)
-articlesRoute.get('/', getAllAdoptionHandler)
-articlesRoute.put('/update/:id', updateAdoptionHandler)
-articlesRoute.delete('/delete/:id', deleteAdoptionHandler)
+adoptionRoute.post('/register', createAdoptionHandler)
+adoptionRoute.get('/', getAllAdoptionHandler)
+adoptionRoute.put('/update/:id', updateAdoptionHandler)
+adoptionRoute.delete('/delete/:id', deleteAdoptionHandler)
 
 
 module.exports = adoptionRoute
