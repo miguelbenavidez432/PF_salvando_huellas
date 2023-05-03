057e0f793a0d1c6da262dd768331e18eb969d594
miguelbenavidez432 <119986047+miguelbenavidez432@users.noreply.github.com>
2023-05-03 01:36:26 -0300

    Fix deply

--- Back/src/routes/postsRoute.js
+++ Back/src/routes/postsRoute.js
@@ -6,16 +6,12 @@ const {
     updatPostHandler,
     deletePostHandler,
 } = require('../handlers/postsHandler')
-const authjwt = require(newFunction())
+const authjwt = require('../Middleware/authjwt')
 
 postsRoute.get('/', getAllPostsHandler)
 postsRoute.get('/:id', getPostByIdHandler)
-postsRoute.post('/register', /*authjwt.authjwt,*/ createPostHandler)
+postsRoute.post('/register', authjwt.authjwt, createPostHandler)
 postsRoute.put('/update/:id', updatPostHandler)
 postsRoute.delete('/delete/:id', deletePostHandler)
 
 module.exports = postsRoute
-
-function newFunction() {
-    return '../middleware/authjwt'
-}
